d1361d761f0c20c6be90cd8185aa7952982510f0
ProgrammerJayT <theanthem8@gmail.com>
2024-08-25 17:02:59 +0200

    Create access control group(s) entity

---
+++ app/Http/Controllers/AccessControlGroupController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AccessControlGroup;
+use App\Http\Requests\StoreAccessControlGroupRequest;
+use App\Http\Requests\UpdateAccessControlGroupRequest;
+
+class AccessControlGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAccessControlGroupRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AccessControlGroup $accessControlGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AccessControlGroup $accessControlGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAccessControlGroupRequest $request, AccessControlGroup $accessControlGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AccessControlGroup $accessControlGroup)
+    {
+        //
+    }
+}
