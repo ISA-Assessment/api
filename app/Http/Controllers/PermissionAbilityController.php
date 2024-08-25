a213f6988cec7bf0004b66e580259fafa1cc0394
ProgrammerJayT <theanthem8@gmail.com>
2024-08-25 17:45:56 +0200

    Create permission ability entity

---
+++ app/Http/Controllers/PermissionAbilityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PermissionAbility;
+use App\Http\Requests\StorePermissionAbilityRequest;
+use App\Http\Requests\UpdatePermissionAbilityRequest;
+
+class PermissionAbilityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePermissionAbilityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PermissionAbility $permissionAbility)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PermissionAbility $permissionAbility)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePermissionAbilityRequest $request, PermissionAbility $permissionAbility)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PermissionAbility $permissionAbility)
+    {
+        //
+    }
+}
