f3fa87891ae6a5d0e9deded8102f2b505978952a
ProgrammerJayT <theanthem8@gmail.com>
2024-08-25 17:36:23 +0200

    Update access control group(s) table migration

--- database/migrations/2024_08_25_145528_create_access_control_groups_table.php
+++ database/migrations/2024_08_25_145528_create_access_control_groups_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('access_control_groups', function (Blueprint $table) {
-            $table->id();
+            $table->integer('access_control_group_id')->autoIncrement();
+            $table->string('group');
             $table->timestamps();
         });
     }
