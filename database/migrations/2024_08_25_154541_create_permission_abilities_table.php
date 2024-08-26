63f779e2d244375709f75409c06186ed057237f4
ProgrammerJayT <theanthem8@gmail.com>
2024-08-27 00:22:12 +0200

    Update columns in migration

--- database/migrations/2024_08_25_154541_create_permission_abilities_table.php
+++ database/migrations/2024_08_25_154541_create_permission_abilities_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('permission_abilities', function (Blueprint $table) {
-            $table->id();
+            $table->integer('permission_ability_id')->autoIncrement();
+            $table->string('ability');
             $table->timestamps();
         });
     }
